cca59f8c5b178db81d133a5b8b6bd8aac922651f
bharath532 <bharath978930@gmail.com>
2025-09-01 15:07:32 +0530

    CHANGED

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 import express from "express";
 import bcrypt from "bcrypt";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
